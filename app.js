37a6c68418ba5ff21447e92e453b38f0c15b8da2
S2 <s@ireoo.com>
2018-01-27 13:18:36 +0800

    init

--- app.js
+++ app.js
@@ -2,22 +2,28 @@ const fs = require('fs');
 const path = require('path');
 const ROOT = process.cwd();
 
-const dir = path.join(ROOT, 'libs')
-const dirList = fs.readdirSync(dir);
+const getLib = (ROOT = process.cwd()) => {
 
-let libs = {};
+    const dir = path.join(ROOT, 'libs')
+    const dirList = fs.readdirSync(dir);
 
-dirList.forEach((file) => {
-    let lib = require(path.join(dir, file));
-    if (typeof (lib) === 'object') {
-        for (let l in lib) {
-            libs[l] = lib[l];
+    let libs = {};
+
+    dirList.forEach((file) => {
+        let lib = require(path.join(dir, file));
+        if (typeof(lib) === 'object') {
+            for (let l in lib) {
+                libs[l] = lib[l];
+            }
+        } else if (typeof(lib) === 'function' || typeof(lib) === 'number' || typeof(lib) === 'string') {
+            libs[file.split('.')[0]] = lib;
+        } else {
+            console.log(`The file "${file}" does not contain exports property or modules.exports property, it is also possible that this file is not a valid lib file!!!`);
         }
-    } else if (typeof (lib) === 'function' || typeof (lib) === 'number' || typeof (lib) === 'string') {
-        libs[file.split('.')[0]] = lib;
-    } else {
-        console.log(`The file "${file}" does not contain exports property or modules.exports property, it is also possible that this file is not a valid lib file!!!`);
-    }
-});
+    });
 
-exports = module.exports = libs;
+    return libs;
+
+}
+
+exports = module.exports = getLib;
